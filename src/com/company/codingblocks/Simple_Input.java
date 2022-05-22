458c2677f18c3e49ad847dec7edc8f62548a057a
ajaysharma12799 <ajaysharma12799@gmail.com>
2022-05-22 16:52:53 +0530

    Simple Input

---
+++ src/com/company/codingblocks/Simple_Input.java
@@ -0,0 +1,28 @@
+package com.company.codingblocks;
+
+import java.util.Scanner;
+
+public class Simple_Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int sum = 0;
+
+        while(sc.hasNextInt())
+        {
+            n = sc.nextInt();
+            sum = sum + n;
+            if(n > -1000 && n < 1000)
+            {
+                if(sum >= 0)
+                {
+                    System.out.println(n);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
